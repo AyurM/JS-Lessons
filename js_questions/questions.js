5feb8327b50223914890c134bfd09ab6cbf0b0d9
AyurM <ayurmarh@gmail.com>
2021-04-13 09:10:35 +0800

    Добавлено задание "FAQ"

---
+++ js_questions/questions.js
@@ -0,0 +1,26 @@
+window.onload = init;
+
+let index = -1;
+let arrowImages, answerDivs;
+
+function init() {
+  arrowImages = document.querySelectorAll("button img");
+  answerDivs = document.querySelectorAll(".answer");
+}
+
+function onArrowClick(i) {
+  if (index == i) {
+    arrowImages[i].style.transform = "rotate(0deg)";
+    answerDivs[i].style.display = "none";
+    index = -1;
+  } else {
+    if (index != -1) {
+      arrowImages[index].style.transform = "rotate(0deg)";
+      answerDivs[index].style.display = "none";
+    }
+
+    index = i;
+    arrowImages[i].style.transform = "rotate(180deg)";
+    answerDivs[i].style.display = "block";
+  }
+}
